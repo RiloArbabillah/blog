f29537c4d33de189b4e977ccdae3e6a6e0107c09
RiloArbabillah <rilo.arbabillah@gmail.com>
2022-11-24 01:31:55 +0800

    rabu

--- database/seeders/SiswaSeeder.php
+++ database/seeders/SiswaSeeder.php
@@ -9,6 +9,38 @@ class SiswaSeeder extends Seeder
 {
     public function run()
     {
+        // list data siswa
+        $data_siswa = [
+            [
+                'nama' => 'Alief Wafiudin',
+                'kelas' => 'XI PPLG',
+                'umur' => random_int(15, 30),
+            ],
+            [
+                'nama' => 'Anggara Prima',
+                'kelas' => 'XI PPLG',
+                'umur' => random_int(15, 30),
+            ],
+            [
+                'nama' => 'Nenni Destiani',
+                'kelas' => 'XI PPLG',
+                'umur' => random_int(15, 30),
+            ],
+        ];
+
+        // Proses Hapus data
+        DB::table('siswa')->truncate();
+
+        // proses simpan data
+        foreach ($data_siswa as $data) {
+            DB::table('siswa')->insert([
+                'nama' => $data['nama'],
+                'kelas' => $data['kelas'],
+                'umur' => $data['umur'],
+            ]);
+        }
+
+        // Proses ambil data dari tabel siswa
         $siswa = DB::table('siswa')->get();
         dd($siswa);
     }
---
+++ resources/views/content.blade.php
@@ -0,0 +1,33 @@
+@extends('layout')
+@section('content')
+
+<div class="bg-gray-300">
+    <div class="max-w-7xl mx-auto bg-white flex flex-col px-10 min-h-screen py-10 gap-10">
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci fugit aut odio recusandae eligendi corporis accusamus quos cumque. Illo perspiciatis doloribus maxime molestiae obcaecati dolorum eveniet veritatis quisquam, possimus pariatur!
+            </span>
+        </div>
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci fugit aut odio recusandae eligendi corporis accusamus quos cumque. Illo perspiciatis doloribus maxime molestiae obcaecati dolorum eveniet veritatis quisquam, possimus pariatur!
+            </span>
+        </div>
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci fugit aut odio recusandae eligendi corporis accusamus quos cumque. Illo perspiciatis doloribus maxime molestiae obcaecati dolorum eveniet veritatis quisquam, possimus pariatur!
+            </span>
+        </div>
+        <div class="">
+            <h1 class="text-lg font-semibold">Judul</h1>
+            <span class="text-sm">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci fugit aut odio recusandae eligendi corporis accusamus quos cumque. Illo perspiciatis doloribus maxime molestiae obcaecati dolorum eveniet veritatis quisquam, possimus pariatur!
+            </span>
+        </div>
+    </div>
+</div>
+
+@endsection
